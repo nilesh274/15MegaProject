bad62c8dcc4011524b270a618db9d73b61fc6872
nilesh274 <nileshparmarofficial274@gmail.com>
2024-09-20 22:20:14 +0530

    after deploy

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AboutUs = () => {
     return (
         <>
-            <div className='px-10 py-9 h-full bg-white shadow-lg dark:bg-black '>
+            <div className='mx-auto px-5 py-10 h-full bg-white shadow-lg dark:bg-black '>
                 <div className="@container">
                     <div className="pb-8">
                         <div
